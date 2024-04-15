cdb4e3820b43033bf87163351e27b8e8bcda5567
unknown <yurinicoliche2003@gmail.com>
2024-04-15 12:57:23 -0300

    corrigindo

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const loop = setInterval(() => {
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`
-        mario.src = './img/game-over.png'
+        mario.src = 'img/game-over.png'
         mario.style.width = '75px'
         mario.style.left = '50px'
 
